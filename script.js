cd9924fc374f6a6bd528f4263596664c132ac6e4
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-12 15:02:18 -0400

    complete Async P1 Number Facts

---
+++ script.js
@@ -0,0 +1,49 @@
+//Use promises to work with APIs
+
+BASE_URL = "http://numbersapi.com/";
+BASE_JSON = "?json";
+
+//1. Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API. Details.
+
+async function getFavNumberFact(num) {
+  let res = await axios.get(`${BASE_URL}${num}${BASE_JSON}`);
+  console.log(res.data.text);
+}
+
+// 2. Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
+
+const fourNumArray = [3, 5, 7, 9];
+const fourNumPromises = [];
+
+for (let i = 0; i < fourNumArray.length; i++) {
+  fourNumPromises.push(axios.get(`${BASE_URL}${fourNumArray[i]}${BASE_JSON}`));
+}
+async function parallelAsync() {
+  try {
+    let numbers = await Promise.all(fourNumPromises);
+    numbers.forEach((val) => $("body").append(`<p>${val.data.text}</p>`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+parallelAsync();
+
+// //3 Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
+
+let favoriteNumber = "11";
+
+const fourFactPromises = [];
+
+for (let i = 0; i < fourNumArray.length; i++) {
+  fourFactPromises.push(axios.get(`${BASE_URL}${favoriteNumber}${BASE_JSON}`));
+}
+
+async function parallelFavAsync() {
+  try {
+    let numbers = await Promise.all(fourFactPromises);
+    numbers.forEach((val) => $("body").append(`<p>${val.data.text}</p>`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+parallelFavAsync();
